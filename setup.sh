a4df7560f6974f471f4af9dd193abde42067de8a
denverbaumgartner <denver@semiotic.ai>
2024-01-28 05:04:11 +0000

    data: prep

--- setup.sh
+++ setup.sh
@@ -4,8 +4,16 @@ sudo apt-get install libbz2-dev
 sudo apt-get install libreadline-dev
 sudo apt-get install libssl-dev
 
+pyenv install 3.10.12
+pyenv local 3.10.12
+
 python3 -m venv venv 
 source venv/bin/activate
 pip install ipykernel
 pip install jupyter
-python -m ipykernel install --user --name=myenvkernel --display-name=sql-dataset-tuning
+pip install transformers
+pip install pytorch_lightning==0.7.5
+pip install datasets
+pip install nltk
+pip install SentencePiece
+python -m ipykernel install --user --name=sql_dataset_tuning --display-name=sql_dataset_tuning
